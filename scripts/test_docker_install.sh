8731335944bc92d1ec75ec81b8e857d827d3d4d5
Nick Hynes <nhynes@oasislabs.com>
2019-09-12 18:11:26 +0000

    Slightly improve test_docker_install.sh

--- scripts/test_docker_install.sh
+++ scripts/test_docker_install.sh
@@ -3,4 +3,8 @@
 cd $(git rev-parse --show-toplevel)/.circleci/docker
 docker build . -t installer_tester:latest
 cd ../../
-docker run -it --rm -v $(pwd):/mnt installer_tester python /mnt/installer.py
+docker_extra_cmd="exit 0"
+if [ $1 == "-i" ]; then
+    docker_extra_cmd="bash"
+fi
+docker run -it --rm -v $(pwd):/mnt installer_tester python /mnt/installer.py && $docker_extra_cmd
